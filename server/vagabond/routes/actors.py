9adc8806bf424e69fac9530fa8afb320c7eab725
Jaeyong Sin <jaeyongsin@ufl.edu>
2021-03-10 00:07:51 +0000

    add new actor and switch between existing actors

--- server/vagabond/routes/actors.py
+++ server/vagabond/routes/actors.py
@@ -3,7 +3,8 @@ from math import ceil
 from flask import make_response, request
 
 from vagabond.__main__ import app, db
-from vagabond.models import Actor, Following
+from vagabond.models import Actor, User, Following
+from vagabond.routes import require_signin
 from vagabond.crypto import require_signature
 from vagabond.routes import error
 from vagabond.config import config
@@ -21,7 +22,43 @@ def route_get_actor_by_username(username):
     response.headers['Content-Type'] = 'application/activity+json'
     return response
 
+@app.route('/api/v1/newactor')
+@require_signin
+def route_add_new_actor(username):
+    actor_name = request.get_json().get('actorName')
 
+    user = db.session.query(User).filter(db.func.lower(User.username) == db.func.lower(username)).first()
+
+    existing_actor = db.session.query(Actor).filter(db.func.lower(Actor.username) == db.func.lower(actor_name)).first()
+    if existing_actor is not None:
+        return error('That actor name is not available.', 404)
+    
+    new_actor = Actor(actor_name, user_id=user.id)
+    db.session.add(new_actor)
+    db.session.flush()
+
+    db.session.commit()
+    
+    response = make_response(new_actor.to_dict(), 200)
+    response.headers['Content-Type'] = 'application/activity+json'
+    return response
+
+@app.route('/api/v1/switchactor')
+@require_signin
+def route_switch_actor(username):
+    user = db.session.query(User).filter(db.func.lower(User.username) == db.func.lower(username)).first()
+
+    actor = db.session.query(Actor).filter(db.func.lower(Actor.username) == db.func.lower(username)).first()
+    if actor is None:
+        return error('Actor not found', 404)
+    
+    user.primary_actor_id = actor.id
+    db.session.commit()
+
+    response = make_response(actor.to_dict(), 200)
+    response.header['Content-Type'] = 'application/activity+json'
+    return response
+    
 @app.route('/api/v1/actors/<username>/following')
 def route_get_actor_following(username):
     '''
