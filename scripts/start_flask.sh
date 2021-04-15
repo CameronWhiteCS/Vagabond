d1e40f9105645a59ca2725c2055d1b06ee6bc3dc
gerakey <33693472+gerakey@users.noreply.github.com>
2021-04-14 22:01:32 -0400

    Update start_flask.sh

--- scripts/start_flask.sh
+++ scripts/start_flask.sh
@@ -2,4 +2,4 @@ bash server_deploy.sh
 export FLASK_APP=~/vagabond/server/vagabond/__main__.py
 source ~/vagabond/server/vagabond/env/bin/activate
 pip3 install -r ~/vagabond/server/requirements.txt
-flask run --port 6001
+flask run --port 1236
